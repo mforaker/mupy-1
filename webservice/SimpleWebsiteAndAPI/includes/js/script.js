cebc3b78314c15f1981872fcbafb0881e32a5574
Brian Rodgers Vargo <47480412+bethanrv@users.noreply.github.com>
2019-07-24 17:16:27 -0400

    css and js files

---
+++ webservice/SimpleWebsiteAndAPI/includes/js/script.js
@@ -0,0 +1,10 @@
+//Navigation Test via js href
+//+jquery to detect button press
+$("#loginBtn").click(
+function(){
+	window.location.href = 'test.html';
+});
+
+function test(){
+	alert('success');
+}
