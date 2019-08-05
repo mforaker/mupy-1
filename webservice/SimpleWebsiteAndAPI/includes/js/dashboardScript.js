0c75b243e90265010647b768f57203ad5f648b89
Brian Rodgers Vargo <47480412+bethanrv@users.noreply.github.com>
2019-08-05 13:05:48 -0400

    Add files via upload

---
+++ webservice/SimpleWebsiteAndAPI/includes/js/dashboardScript.js
@@ -0,0 +1,19 @@
+//JS for dashboard.html
+
+$("#courseDropdown").click(
+function(){
+	alert("+++");
+});
+
+ $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
+
+$("#dashboard-logo-container").click(
+function(){
+	window.location.href = '../index.html';
+});
+
+
